728c9b23f9e7d1613f3087ea9b88d3903660c23c
Rockay98 <filipepereira9855@gmail.com>
2024-03-14 17:27:54 +0000

    Bonus: The spawn interval is always the same
    Fixed

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,13 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float minSpawnInterval = 3.0f;
+    private float maxSpawnInterval = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(minSpawnInterval, maxSpawnInterval));
     }
 
     // Spawn random ball at random x position at top of play area
